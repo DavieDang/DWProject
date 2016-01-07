a30ab4266edbcb4bf992ca7332c496272ec46096
DavieDang <mac@macdemacbook-pro.local>
2016-01-07 11:59:45 +0800

    Initial Commit

---
+++ DWProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DWProject
+//
+//  Created by Mac on 16/1/7.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DWProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DWProject
+//
+//  Created by Mac on 16/1/7.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
